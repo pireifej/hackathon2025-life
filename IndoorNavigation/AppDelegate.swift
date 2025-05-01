6f7661b6b2128d935514b9b981d55f28d61e07f2
Paul Ireifej <pireifej@gmail.com>
2025-05-01 14:10:56 -0400

    changes

---
+++ IndoorNavigation/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+import SwiftUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let contentView = ContentView()
+            .environmentObject(LocationViewModel())
+            .environmentObject(NavigationViewModel())
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = UIHostingController(rootView: contentView)
+        self.window = window
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+} 
